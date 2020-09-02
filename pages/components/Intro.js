8df133a0fc886d6531e0c8a5437065a370a369fd
PAA-KOFI AIDOO <paakofiaidoo17@gmail.com>
2020-09-02 00:14:40 +0000

    addition of animation

---
+++ pages/components/Intro.js
@@ -0,0 +1,202 @@
+import React, { Component } from "react";
+import Link from "next/link";
+import styles from "../../styles/Intro.module.css";
+
+class Intro extends Component {
+	constructor(props) {
+		super(props);
+		this.intro = React.createRef();
+		this.avatar = React.createRef();
+		this.state = {};
+	}
+
+	componentDidMount() {
+		console.log(this.intro, this.avatar);
+		this.intreact();
+		console.log(this.state);
+	}
+
+	intreact = () => {
+		console.log(this.intro.current, this.avatar.current);
+		var container = this.intro.current;
+		var inner = this.avatar.current;
+		let newStyle = "rotateX(0.05deg) rotateY(-0.24deg)";
+
+		var mouse = {
+			_x: 0,
+			_y: 0,
+			x: 0,
+			y: 0,
+			updatePosition: function (event) {
+				var e = event || window.event;
+				this.x = e.clientX - this._x;
+				this.y = (e.clientY - this._y) * -1;
+			},
+			setOrigin: function (e) {
+				this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+				this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+			},
+			show: function () {
+				return "(" + this.x + ", " + this.y + ")";
+			},
+		};
+
+		// Track the mouse position relative to the center of the container.
+		mouse.setOrigin(container);
+
+		//----------------------------------------------------
+
+		var counter = 0;
+		var refreshRate = 10;
+		var isTimeToUpdate = function () {
+			return counter++ % refreshRate === 0;
+		};
+
+		//----------------------------------------------------
+
+		var onMouseEnterHandler = function (event) {
+			update(event);
+		};
+
+		var onMouseLeaveHandler = function () {
+			inner.style = "";
+		};
+
+		var onMouseMoveHandler = function (event) {
+			if (isTimeToUpdate()) {
+				update(event);
+			}
+		};
+
+		//----------------------------------------------------
+
+		var update = function (event) {
+			mouse.updatePosition(event);
+			updateTransformStyle(
+				(mouse.y / inner.offsetHeight / 2).toFixed(2),
+      (mouse.x / inner.offsetWidth / 2).toFixed(2)
+			);
+		};
+
+		var updateTransformStyle = function (x, y) {
+
+			var style =
+				"rotateX(" +
+				angleTransform(x) +
+				"deg) rotateY(" +
+				angleTransform(y) +
+				"deg)";
+			newStyle = style;
+			setStyle(style);
+		};
+		let angleTransform = (val) => {
+			val = parseFloat(val)*40 ;
+			if (val < 0) {
+				return val;
+			} else {
+				return val;
+			}
+		};
+
+		//--------------------------------------------------------
+		let setStyle = (newStyle) => {
+			console.log(newStyle);
+			this.setState({ transform: newStyle });
+		};
+
+		container.onmousemove = onMouseMoveHandler;
+		container.onmouseleave = onMouseLeaveHandler;
+		container.onmouseenter = onMouseEnterHandler;
+	};
+
+	render() {
+		return (
+			<div id="intro" className={styles.intro} ref={this.intro}>
+				<div>
+					<img
+						src="/brand/vector/default-monochrome.svg"
+						alt="profile pic"
+						className={styles.avatar}
+						ref={this.avatar}
+                        style={this.state}
+                        load="lazy"
+					/>
+					<div className={styles.details}>
+						<ul>
+							<li>
+								<Link href="https://web.facebook.com/paakofi.aidoo">
+									<img
+										src="/facebook.svg"
+										alt="facebook"
+										className={styles.contact}
+									/>
+								</Link>
+							</li>
+							<li>
+								<Link href="https://www.instagram.com/pk_aidoo">
+									<img
+										src="/instagram.svg"
+										alt="instagram"
+										className={styles.contact}
+									/>
+								</Link>
+							</li>
+							<li>
+								<Link href="https://twitter.com/PaaKofiaidoo2">
+									<img
+										src="/twitter.svg"
+										alt="twitter"
+										className={styles.contact}
+									/>
+								</Link>
+							</li>
+							<li>
+								<Link href="https://www.linkedin.com/in/paa-kofi-aidoo-a98036183/">
+									<img
+										src="/linkedin.svg"
+										alt="linkedin"
+										className={styles.contact}
+									/>
+								</Link>
+							</li>
+							<li>
+								<Link href="https://wa.me/233202396856">
+									<img
+										src="/whatsapp.svg"
+										alt="whatsapp"
+										className={styles.contact}
+									/>
+								</Link>
+							</li>
+							<li>
+								<Link href="https://github.com/paakofiaidoo">
+									<img
+										src="/github.svg"
+										alt="github"
+										className={styles.contact}
+									/>
+								</Link>
+							</li>
+							<li>
+								<Link href="mailto:paakofiaidoo17@gmail.com">
+									<img src="/mail.svg" alt="email" className={styles.contact} />
+								</Link>
+							</li>
+							<li>
+								<Link href="tel:+233202396856">
+									<img
+										src="/telephone.svg"
+										alt="call"
+										className={styles.contact}
+									/>
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Intro;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import Intro from "./components/Intro"
 
-export default function Home() {
+const Home = () => {
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -10,127 +10,98 @@ export default function Home() {
 				<link rel="icon" href="/brand/cover.png" />
 			</Head>
 			<main className={styles.main}>
-				<Intro />
+				<div className={styles.overlay}>
+					<Intro />
+				</div>
 			</main>
 		</div>
 	);
-}
-
-const Intro = () => {
-	return (
-		<div id="intro" className={styles.intro}>
-			<div>
-				<img
-					src="/brand/vector/default-monochrome.svg"
-					alt="profile pic"
-					className={styles.avatar}
-				/>
-				<div className={styles.details}>
-					<ul>
-						<li>
-							<Link href="https://web.facebook.com/paakofi.aidoo">
-								<img
-									src="/facebook.svg"
-									alt="facebook"
-									className={styles.contact}
-								/>
-							</Link>
-						</li>
-						<li>
-							<Link href="https://www.instagram.com/pk_aidoo">
-								<img
-									src="/instagram.svg"
-									alt="instagram"
-									className={styles.contact}
-								/>
-							</Link>
-						</li>
-						<li>
-							<Link href="https://twitter.com/PaaKofiaidoo2">
-								<img
-									src="/twitter.svg"
-									alt="twitter"
-									className={styles.contact}
-								/>
-							</Link>
-						</li>
-						<li>
-							<Link href="https://www.linkedin.com/in/paa-kofi-aidoo-a98036183/">
-								<img
-									src="/linkedin.svg"
-									alt="linkedin"
-									className={styles.contact}
-								/>
-							</Link>
-						</li>
-						<li>
-							<Link href="https://wa.me/233202396856">
-								<img
-									src="/whatsapp.svg"
-									alt="whatsapp"
-									className={styles.contact}
-								/>
-							</Link>
-						</li>
-						<li>
-							<Link href="https://github.com/paakofiaidoo">
-								<img
-									src="/github.svg"
-									alt="github"
-									className={styles.contact}
-								/>
-							</Link>
-						</li>
-						<li>
-							<Link href="mailto:paakofiaidoo17@gmail.com">
-								<img src="/mail.svg" alt="email" className={styles.contact} />
-							</Link>
-						</li>
-						<li>
-							<Link href="tel:+233202396856">
-								<img
-									src="/telephone.svg"
-									alt="call"
-									className={styles.contact}
-								/>
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	);
 };
 
-const About = () => {
-	return (
-		<div id="about" className={styles.about}>
-			<h2>About Me</h2>
-			<li>Bsc Physics (level 300 Undergrad, KNUST, Kumasi, Ghana)</li>
-			<li>Junior FrontEnd Web Developer (HTM5, CSS, JS , react, nextjs)</li>
-			<li>Junior C++ programer</li>
-			<li>Beginner to IOT</li>
-			<li>Electronics hobbyist</li>
-			<li>Afrodew (Embedded System Team member)</li>
-			<p className={styles.bio}>
-				<h4>Bio : </h4>
-				<p>
-					I was born on the 4th feb, 2000 and since then I have been fascinated
-					about computers and electronics. I started programing with python at
-					the age of 16 but gave up because I got confused at some point but not
-					wanting to give up on my dream as a programer I forced to go to a web
-					dev class at Kumasi Hive when I was in first year at KNUST where I met
-					my mentor Gtime, from there I kept climbing the ladder to the point I
-					am at today and I am still persevering.
-				</p>
-				<p>
-					I live with the presuit of knowledge, knowing that since the universe
-					is infinite knowledge is also infinite. I am always glad to learn
-					something new and I try to always solve problems rather than creating
-					them. I have phlegmatic temperament with and introvert personality
-					with a mind full of questions which don't conform with the status-co
-				</p>
-			</p>
-		</div>
-	);
-};
+
+	
+
+	// (function() {
+	// 	// Init
+	// 	intro = intro.current
+	// 		avatar = avatar.current
+	// 		console.log(intro, avatar);
+
+	// 	// Mouse
+	// 	// var mouse = {
+	// 	//   _x: 0,
+	// 	//   _y: 0,
+	// 	//   x: 0,
+	// 	//   y: 0,
+	// 	//   updatePosition: function(event) {
+	// 	// 	var e = event || window.event;
+	// 	// 	this.x = e.clientX - this._x;
+	// 	// 	this.y = (e.clientY - this._y) * -1;
+	// 	//   },
+	// 	//   setOrigin: function(e) {
+	// 	// 	  console.log(e);
+	// 	// 	 this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+	// 	// 	 this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+	// 	//   },
+	// 	//   show: function() {
+	// 	// 	return "(" + this.x + ", " + this.y + ")";
+	// 	//   }
+	// 	// };
+
+	// 	// // Track the mouse position relative to the center of the intro.
+	// 	// mouse.setOrigin(intro);
+
+	// 	// //----------------------------------------------------
+
+	// 	// var counter = 0;
+	// 	// var refreshRate = 10;
+	// 	// var isTimeToUpdate = function() {
+	// 	//   return counter++ % refreshRate === 0;
+	// 	// };
+
+	// 	// //----------------------------------------------------
+
+	// 	// var onMouseEnterHandler = function(event) {
+	// 	//   update(event);
+	// 	// };
+
+	// 	// var onMouseLeaveHandler = function() {
+	// 	//   avatar.style = "";
+	// 	// };
+
+	// 	// var onMouseMoveHandler = function(event) {
+	// 	//   if (isTimeToUpdate()) {
+	// 	// 	update(event);
+	// 	//   }
+	// 	// };
+
+	// 	// //----------------------------------------------------
+
+	// 	// var update = function(event) {
+	// 	//   mouse.updatePosition(event);
+	// 	//   updateTransformStyle(
+	// 	// 	(mouse.y / avatar.offsetHeight / 2).toFixed(2),
+	// 	// 	(mouse.x / avatar.offsetWidth / 2).toFixed(2)
+	// 	//   );
+	// 	// };
+
+	// 	// var updateTransformStyle = function(x, y) {
+	// 	//   var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+	// 	//   avatar.style.transform = style;
+	// 	//   avatar.style.webkitTransform = style;
+	// 	//   avatar.style.mozTranform = style;
+	// 	//   avatar.style.msTransform = style;
+	// 	//   avatar.style.oTransform = style;
+	// 	// };
+
+	// 	// //--------------------------------------------------------
+
+	// 	// intro.onmousemove = onMouseMoveHandler;
+	// 	// intro.onmouseleave = onMouseLeaveHandler;
+	// 	// intro.onmouseenter = onMouseEnterHandler;
+	//   })();
+
+	
+
+
+export default Home;
