ccef657a9557ee5c731a8fc0e061f10cbc893f0d
PAA-KOFI AIDOO <paakofiaidoo17@gmail.com>
2020-09-09 14:09:06 +0000

    completed browser interface

--- pages/components/Browser.js
+++ pages/components/Browser.js
@@ -33,71 +33,77 @@ function Browser(props) {
 			isActive: false,
 			icon: "/navIcons/resume.svg",
 		},
-    ]);
-    
-    const [window, setWindow] = useState({isActive:true})
+	]);
+
+	const [window, setWindow] = useState({ isActive: true });
 
 	const setNavActive = (id) => {
-		setNav(
-			nav.map((menu) => {
-				console.log(menu);
+		setNav((prevNav) => {
+			return prevNav.map((menu) => {
 				if (menu.id === id) {
 					menu.isActive = true;
 				} else {
 					menu.isActive = false;
 				}
 				return menu;
-			})
+			});
+		});
+	};
+	const setWindowActive = (state) => {
+		setWindow({ isActive: state });
+	};
+
+	const Window = (content) => {
+		return (
+			<div className={styles.browser}>
+				<div className={styles.overlay}>
+					<div className={styles.nav}>
+						<ul>
+							{nav.map((menu, index) => (
+								<li
+									className={menu.isActive ? styles.isActive : styles.notActive}
+									key={index}
+								>
+									<a onClick={setNavActive.bind(this, menu.id)}>
+										<img
+											src={menu.icon}
+											className={styles.navIcon}
+											alt={menu.title}
+										/>
+										{menu.title}
+									</a>
+								</li>
+							))}
+						</ul>
+						<span
+							className={styles.cancel}
+							onClick={setWindowActive.bind(this, false)}
+						></span>
+					</div>
+					<div className={styles.content}>{content}</div>
+				</div>
+			</div>
 		);
-    };
-    const setWindowActive=( state)=>{
-        setWindow({isActive:state})
-    }
-    
-    
-    const Window = (content) => {
-        return (
-            <div className={styles.browser}>
-                <div className={styles.nav}>
-                    <ul>
-                        {nav.map((menu, index) => (
-                            <li
-                                className={menu.isActive ? styles.isActive : styles.notActive}
-                                key={index}
-                            >
-                                <a onClick={setNavActive.bind(this, menu.id)}>
-                                    <img
-                                        src={menu.icon}
-                                        className={styles.navIcon}
-                                        alt={menu.title}
-                                    />
-                                    {menu.title}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                    <span className={styles.cancel} onClick={setWindowActive.bind(this, false)} >x</span>
-                </div>
-                <div className={styles.content}>{content}</div>
-            </div>
-        );
-    };
-    const Icon = () =>{
-        return(
-            <div onClick={setWindowActive.bind(this, true)} className={styles.background}>
-                <img src="/brand/logo.svg" alt="Paa-Kofi Aidoo" className={styles.backIcon}/>
-                <p>Paa-Kofi Aidoo</p>
-            </div>
-        )
+	};
+	const Icon = () => {
+		return (
+			<div
+				onClick={setWindowActive.bind(this, true)}
+				className={styles.background}
+			>
+				<div className={styles.overlay}>
+					<img
+						src="/brand/logo.svg"
+						alt="Paa-Kofi Aidoo"
+						className={styles.backIcon}
+					/>
+					<p>Paa-Kofi Aidoo</p>
+				</div>
+			</div>
+		);
+	};
 
-    }
-
-	return (
-		<div>
-			{window.isActive?Window(props.children):Icon()}
-		</div>
-	);
+	return window.isActive ? Window(props.children) : Icon();
 }
-
 
 export default Browser;
