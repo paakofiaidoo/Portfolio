19b5959a524bbed8ff24943d236e28269f7ec162
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-07-24 16:05:25 +0000

    set-up Layout file

---
+++ pages/components/Layout.js
@@ -0,0 +1,18 @@
+import Nav from "./Nav";
+import Head from "next/head";
+import styles from "../../styles/Layout.module.css";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Paa-Kofi Aidoo</title>
+        <link rel="icon" href="/brand/logo.svg" height="100%" width="100%" />
+        <meta name="description" content="My portfolio website" />
+      </Head>
+      <main className={`main ${styles.main}`}>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
