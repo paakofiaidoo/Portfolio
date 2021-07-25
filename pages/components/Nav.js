71228051d162a9dfd74fae99f73b4b714e01b153
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-07-25 03:26:41 +0000

    started z-index agor

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -1,62 +1,27 @@
 import Image from "next/image";
 import styles from "../../styles/Nav.module.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-const Nav = () => {
-  const [state, setState] = useState({ isActive: false });
-  const [nav, setNav] = useState([
-    {
-      id: 1,
-      title: "contact",
-      isActive: true,
-      icon: "/navIcons/contact.svg",
-    },
-    {
-      id: 2,
-      title: "skills",
-      isActive: false,
-      icon: "/navIcons/competence.svg",
-    },
-    {
-      id: 3,
-      title: "about me",
-      isActive: false,
-      icon: "/navIcons/man.svg",
-    },
-    {
-      id: 4,
-      title: "projects",
-      isActive: false,
-      icon: "/navIcons/project.svg",
-    },
-    {
-      id: 5,
-      title: "resume",
-      isActive: false,
-      icon: "/navIcons/resume.svg",
-    },
-  ]);
-  const { isActive } = state;
+const Nav = ({ links, open }) => {
   return (
-    <nav
-      className={`navbar ${isActive ? `isActive ${styles.isActive}` : ""} ${
-        styles.navbar
-      }`}
-    >
+    <nav className={`navbar  ${styles.navbar}`}>
       <div className={`links  ${styles.links}`}>
         <>
-          <li className={`${isActive ? styles.isActive : ""}`}>
+          <li>
             <Image
               className={`icon`}
               src="/brand/vector/default-monochrome.svg"
-              alt="karibe Logo"
+              alt="Paa-Kofi Aidoo logo"
               width={60}
               height={60}
             />
           </li>
-          {nav.map(({ title, icon, isActive }, i) => (
-            <li className={`${isActive ? styles.isActive : ""}`}>
+          {links.map(({ title, icon, isActive, id, index }, i) => (
+            <li
+              className={`${isActive ? styles.isActive : ""}`}
+              onClick={() => {
+                open(id);
+              }}
+            >
               <img src={icon} className={styles.linksIcon} alt={title} />
               <a href="#">{title}</a>
             </li>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,80 @@
 import Layout from "./components/Layout";
 import Intro from "./components/Intro";
-import Browser from "./components/Application";
+import Application from "./components/Application";
+import { useState } from "react";
 
 const Home = () => {
+  const [apps, setApps] = useState([
+    {
+      id: 1,
+      title: "contact",
+      isActive: false,
+      icon: "/navIcons/contact.svg",
+      index: 0,
+    },
+    {
+      id: 2,
+      title: "skills",
+      isActive: false,
+      icon: "/navIcons/competence.svg",
+      index: 0,
+    },
+    {
+      id: 3,
+      title: "about me",
+      isActive: false,
+      icon: "/navIcons/man.svg",
+      index: 0,
+    },
+    {
+      id: 4,
+      title: "projects",
+      isActive: false,
+      icon: "/navIcons/project.svg",
+      index: 0,
+    },
+    {
+      id: 5,
+      title: "resume",
+      isActive: false,
+      icon: "/navIcons/resume.svg",
+      index: 0,
+    },
+  ]);
+  const [indexes, setIndexes] = useState([]);
+  const open = (idn) => {
+    const newApps = apps.map(({ id, isActive, title, icon }) => {
+      if (id === idn) {
+        if (!isActive) {
+          isActive = true;
+          setIndexes((cur) => {
+            return [...cur, id];
+          });
+        }
+      }
+      return { id, title, isActive, icon };
+    });
+    setApps(newApps);
+  };
+  const close = (idn) => {
+    const newApps = apps.map(({ id, isActive, title, icon }) => {
+      if (id === idn) {
+        if (isActive) {
+          isActive = false;
+          setIndexes((cur) => {
+            return cur.filter(({ id }) => id !== idn);
+          });
+        }
+      }
+      return { id, title, isActive, icon };
+    });
+    setApps(newApps);
+  };
   return (
-    <Layout>
-      <Browser>
+    <Layout open={open} apps={apps}>
+      <Application close={close}>
         <Intro />
-      </Browser>
+      </Application>
     </Layout>
   );
 };
