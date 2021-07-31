166d78009c15c3b24c3b311716128bb18145da26
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-07-31 01:34:16 +0000

    added apps and started drag fun

--- pages/components/Application.js
+++ pages/components/Application.js
@@ -3,13 +3,36 @@ import { useState, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-function Application({ children, close, layer, app }) {
+function Application({ style, children, close, layer, app, mousePosition }) {
   const { title, icon, id } = app;
+  console.log(children);
+  const [mouse, setMouse] = useState({ initX: 0, initY: 0, dragEvent: false });
+
+  const inputEl = useRef();
 
   const Window = (children) => {
     return (
-      <div className={styles.app} style={{ zIndex: layer + 2 }}>
-        <div className={styles.titleBar}>
+      <div
+        className={styles.app}
+        ref={inputEl}
+        onMove={(e) => {
+          console.log(e.clientY);
+        }}
+        style={{ zIndex: layer + 2, ...style }}
+      >
+        <div
+          onDrag={(e) => {
+            console.log(style);
+          }}
+          onDragEnd={(e) => {
+            console.log("end");
+          }}
+          onDragStart={(e) => {
+            console.log("start");
+          }}
+          className={styles.titleBar}
+          draggable
+        >
           <li className={styles.title}>
             <img src={icon} className={`icon ${styles.icon}`} alt={app.title} />
             {title}
