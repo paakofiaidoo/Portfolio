8d83a93ad7ec206e40802a77e2b1215ba9a9e402
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-08-31 22:59:53 +0000

    setup download feature

--- pages/atoms/Nav.js
+++ pages/atoms/Nav.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../../styles/Nav.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -8,15 +9,23 @@ const Nav = ({ links = [], open }) => {
       <div className={`links  ${styles.links}`}>
         <>
           <li>
-            <Image
-              className={`icon`}
-              src="/brand/vector/default-monochrome.svg"
-              alt="Paa-Kofi Aidoo logo"
-              width={60}
-              height={60}
+            <a
+              style={{ display: "flex" }}
+              href="/brand/cover.png"
+              target="_blank"
+              download
               title="Click to download Resume"
-            />
+            >
+              <Image
+                className={`icon ${styles.linksIcon}`}
+                src="/brand/vector/default-monochrome.svg"
+                alt="Paa-Kofi Aidoo logo"
+                width={60}
+                height={60}
+              />
+            </a>
           </li>
+
           {links.map(({ title, icon, isActive, id, fa }, i) => (
             <li
               className={`${isActive ? styles.isActive : ""}`}
@@ -30,6 +39,7 @@ const Nav = ({ links = [], open }) => {
                   className={` ${styles.linksIcon}`}
                   title={title}
                   icon={fa}
+                  color="#25BBA8"
                 />
               ) : (
                 <img src={icon} className={styles.linksIcon} alt={title} />
