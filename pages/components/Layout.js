c6d8cf90f141585b45d95559369b9c9b224403a0
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-09-02 20:55:29 +0000

    fixed appication layout

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -10,15 +10,17 @@ const Layout = ({ children, apps, open, onMove }) => {
         <link rel="icon" href="/brand/logo.svg" height="100%" width="100%" />
         <meta name="description" content="My portfolio website" />
       </Head>
-      <main
-        className={`main ${styles.main}`}
-        onMouseMove={(e) => {
-          onMove(e);
-        }}
-      >
-        {children}
-      </main>
-      <Nav links={apps} open={open} />
+      <div className={`${styles.body}`}>
+        <main
+          className={`main ${styles.main}`}
+          onMouseMove={(e) => {
+            onMove(e);
+          }}
+        >
+          {children}
+        </main>
+        <Nav links={apps} open={open} />
+      </div>
     </>
   );
 };
