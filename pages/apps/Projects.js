aa60d7b9a46f8745e912fc57e7da659438bb85af
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-09-17 23:40:46 +0000

    added project data

--- pages/apps/Projects.js
+++ pages/apps/Projects.js
@@ -1,10 +1,9 @@
-
 const Projects = () => {
-  return (
-    <div>
-      this is projects
-    </div>
-  );
-}
+  const projects = [
+    { name: "artisanHR", href: "https://artsan-hr.herokuapp.com" },
+    { name: "karibe", href: "karibe-valley-farms.vercel.app/" },
+  ];
+  return <div>this is projects</div>;
+};
 
 export default Projects;
