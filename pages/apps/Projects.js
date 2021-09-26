5a39727100f039b214abf86ab71644199e81b4c3
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-09-26 16:29:45 +0000

    updated project img s

--- pages/apps/Projects.js
+++ pages/apps/Projects.js
@@ -1,7 +1,37 @@
 const Projects = () => {
   const projects = [
-    { name: "artisanHR", href: "https://artsan-hr.herokuapp.com" },
-    { name: "karibe", href: "karibe-valley-farms.vercel.app/" },
+    {
+      name: "artisanHR",
+      href: "https://artsan-hr.herokuapp.com",
+      description: "",
+      img:"/projects/artsan-hr.png"
+    },
+    {
+      name: "karibe",
+      href: "karibe-valley-farms.vercel.app/",
+      description: "",
+      img:"karibe.png"
+    },
+    {
+      name: "nonimous",
+      href: "https://artsan-hr.herokuapp.com",
+      description: "nonimous-chat.png ",
+    },
+    {
+      name: "uv sterilizer",
+      href: "karibe-valley-farms.vercel.app/",
+      description: "",
+    },
+    {
+      name: "Special relativity calculator",
+      href: "https://artsan-hr.herokuapp.com",
+      description: "",
+    },
+    {
+      name: "uv sterilizer",
+      href: "karibe-valley-farms.vercel.app/",
+      description: "",
+    },
   ];
   return <div>this is projects</div>;
 };
