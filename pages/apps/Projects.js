4b5b00ffbc6f37f3042c09fa0cb03060e7349116
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-10-24 23:27:32 +0000

    finished set up phone and laptop ui

--- pages/apps/Projects.js
+++ pages/apps/Projects.js
@@ -1,7 +1,7 @@
 import AwesomeSlider from "react-awesome-slider";
 //import "react-awesome-slider/dist/styles.css";
 import styles from "../../styles/apps/Projects.module.scss";
-
+import stylesCos from "../../styles/apps/ProjectsCos.module.scss";
 const Projects = () => {
   const projects = [
     {
@@ -26,7 +26,7 @@ const Projects = () => {
       name: "uv sterilizer",
       href: "karibe-valley-farms.vercel.app/",
       description: "",
-      img: "/projects/karibe.png",
+      img: "/projects/karibe-valley-farms-vercel-app-480x800phone-08dd37.png",
     },
     {
       name: "Special relativity calculator",
@@ -36,27 +36,51 @@ const Projects = () => {
     },
   ];
   return (
-    <AwesomeSlider
-      fillParent
-      cssModule={[styles]}
-      customContent={<Test />}>
-      {projects.map(({ name, href, description, img }) => {
-        return (
-          <div>
-            <img src={img} alt={name} />
-            <h1>{name}</h1>
-            <p>{description}</p>
+    <>
+      <div className={stylesCos.slide}>
+
+
+        <div id={stylesCos.phoneShadow}></div>
+        <div id={stylesCos.phone}>
+          <div id={stylesCos.back}></div>
+          <div id={stylesCos.front}></div>
+          <div id={stylesCos.camera}></div>
+          <div id={stylesCos.button}></div>
+          <div id={stylesCos.display}>
+            <span className={stylesCos.upperbefore}><img src="/projects/karibe.png" alt="" /></span>
+
           </div>
-        );
-      })}
-    </AwesomeSlider>
-  );
-};
-const Test = () => {
-  return (
-    <div>
-      <h1>this is Skills</h1>
-    </div>
+        </div>
+        <div className={stylesCos.laptop}>
+          <div className={stylesCos.upper}>
+            <span className={stylesCos.upperbefore}><img src="/projects/karibe.png" alt="" /></span>
+          </div>
+          <div className={stylesCos.lower}></div>
+        </div>
+      </div>
+      {/* <AwesomeSlider
+        fillParent
+        cssModule={[styles]}
+        customContent={<Test />}>
+        {projects.map(({ name, href, description, img }) => {
+          return (
+            <div className="slide">
+              <div>
+                <img className="img" src={img} alt={name} />
+                <img src="/projects/karibe.png" alt="" />
+              </div>
+
+              <div data-type="caption">
+                <p>
+                  <h1>{name}</h1>
+                  <p>{description}</p>
+                  <a href={href}>View projects</a></p>
+              </div>
+            </div>
+          );
+        })}
+      </AwesomeSlider> */}
+    </>
   );
 };
 
