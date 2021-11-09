8b308c2cc9e4225386e96aca922095c098780c23
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-11-09 14:25:49 +0000

    completed skills layout

--- pages/apps/Skills.js
+++ pages/apps/Skills.js
@@ -9,152 +9,132 @@ import react from "../../public/animations/reactnative.json";
 import pro from "../../public/animations/programmingcomputer.json";
 
 const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
+	loop: true,
+	autoplay: true,
+	rendererSettings: {
+		preserveAspectRatio: "xMidYMid slice",
+	},
 };
-const Skills = () => {
-  return (
-    <>
-      <div className={`${styles.skills}`}>
-        <Skill
-          services={[
-            {
-              title: "web",
-              content: "lorem",
-              icon: "",
-              animation: web
-            },
-            {
-              title: "web",
-              content: "lorem",
-              icon: "",
-              animation: react
-            },
-            {
-              title: "web",
-              content: "lorem",
-              icon: "",
-              animation: pro
-            },
-            {
-              title: "web",
-              content: "lorem",
-              icon: "",
-              animation: web
-            },
-            {
-              title: "web",
-              content: "lorem",
-              icon: "",
-              animation: react
-            },
-            {
-              title: "web",
-              content: "lorem",
-              icon: "",
-              animation: pro
-            },
-          ]}
-        />
-      </div>
-    </>
-  );
+
+const Skills = ({ services = [] }) => {
+	const [active, setActive] = useState(0);
+	services = [
+		{
+			title: "web",
+			content:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quam debitis totam veritatis aspernatur facilis, velit porro saepe et eligendi a molestiae ullam quasi rem aliquam nihil, odit aut minus?",
+			icon: "",
+			animation: web,
+		},
+		{
+			title: "web",
+			content:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quam debitis totam veritatis aspernatur facilis, velit porro saepe et eligendi a molestiae ullam quasi rem aliquam nihil, odit aut minus?",
+			icon: "",
+			animation: react,
+		},
+		{
+			title: "web",
+			content:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quam debitis totam veritatis aspernatur facilis, velit porro saepe et eligendi a molestiae ullam quasi rem aliquam nihil, odit aut minus?",
+			icon: "",
+			animation: pro,
+		},
+		{
+			title: "web",
+			content: "lorem",
+			icon: "",
+			animation: web,
+		},
+		{
+			title: "web",
+			content: "lorem",
+			icon: "",
+			animation: react,
+		},
+		{
+			title: "web",
+			content: "lorem",
+			icon: "",
+			animation: pro,
+		},
+	];
+	return (
+		<div className={`${styles.skills}`}>
+			<div className={`${styles.options} `}>
+				{services.map(({ title, animation, content, fa, icon }, i) => {
+					return (
+						<>
+							<div
+								className={`${styles.option} ${i === active ? styles.active : ""}`}
+								key={i}
+								onClick={(e) => {
+									setActive(i);
+								}}>
+								<Lottie
+									options={{ ...defaultOptions, animationData: animation }}
+									height={"100%"}
+									width={"100%"}
+									className={`${styles.anim} ${i === active ? styles.active : ""}`}
+									style={{ display: i === active ? "" : "none" }}
+								/>
+								<div className={`${styles.label} ${styles.shadow}`}>
+									<div className={`${styles.icon}`} style={{ display: i === active ? "none" : "" }}>
+										<Lottie options={{ ...defaultOptions, animationData: animation }} height={"100%"} width={"100%"} />
+									</div>
+
+									<div className={`${styles.info} `}>
+										<div className={`${styles.main} `}>{title}</div>
+										<div className={`${styles.sub} `}>{content}</div>
+									</div>
+								</div>
+							</div>
+						</>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+const Slider = (defaultOptions) => {
+	return (
+		<div className={`${styles.card}`}>
+			<AwesomeSlider bullets organicArrows cssModule={[Slidestyles]}>
+				<div className={`${styles.cardIn}`}>
+					<div className={`anim ${styles.img}`}>
+						<Lottie
+							options={{ ...defaultOptions, animationData: web }}
+							height={"100%"}
+							width={"100%"}
+							style={{ overflow: "visible" }}
+						/>
+					</div>
+					<h2 className={`${styles.title}`}> front end</h2>
+					<p className={`${styles.text}`}>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad voluptatem cumque exercitationem facilis soluta
+						magnam accusamus harum, perferendis consectetur est nostrum voluptate laudantium ex doloribus veritatis,
+						incidunt officiis assumenda in.
+					</p>
+				</div>
+				<div className={`${styles.cardIn}`}>
+					<div className={`anim ${styles.img}`}>
+						<Lottie
+							options={{ ...defaultOptions, animationData: react }}
+							height={"100%"}
+							width={"100%"}
+							style={{ overflow: "visible" }}
+						/>
+					</div>
+					<h2 className={`${styles.title}`}> front end</h2>
+					<p className={`${styles.text}`}>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad voluptatem cumque exercitationem facilis soluta
+						magnam accusamus harum, perferendis consectetur est nostrum voluptate laudantium ex doloribus veritatis,
+						incidunt officiis assumenda in.
+					</p>
+				</div>
+			</AwesomeSlider>
+		</div>
+	);
 };
 
 export default Skills;
-
-
-const Skill = ({ services = [] }) => {
-  const [active, setActive] = useState(0);
-  const [options, setOptions] = useState([
-    {
-      title: "web",
-      content: "something nice about web skills",
-      icon: "",
-    }
-  ])
-
-
-
-  return (
-    <div id="services" >
-      <div className={`${styles.options} `}>
-        {services.map(({ title, animation, content, fa, icon }, i) => {
-          return (
-            <>
-              <div
-                className={`${styles.option} ${i === active ? styles.active : ""}`}
-                key={i}
-
-                onClick={(e) => {
-                  setActive(i);
-                }}>
-                <Lottie
-                  options={{ ...defaultOptions, animationData: animation }}
-                  height={"100%"}
-                  width={"100%"}
-                  className={`${styles.anim} ${i === active ? styles.active : ""}`}
-                  style={{ display: i === active ? "" : "none" }}
-                />
-                <div className={`${styles.label} ${styles.shadow}`}>
-                  <div className={`${styles.icon}`}>
-                    {icon ? <Image src={icon} alt="" width={40} height={40} /> : <FontAwesomeIcon icon={fa} />}
-                  </div>
-
-                  <div className={`${styles.info} `}>
-                    <div className={`${styles.main} `}>{title}</div>
-                    <div className={`${styles.sub} `}>{content}</div>
-                  </div>
-                </div>
-              </div>
-
-            </>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
-const Slider = (defaultOptions) => {
-  return (
-    <div className={`${styles.card}`}>
-      <AwesomeSlider bullets organicArrows cssModule={[Slidestyles]}>
-        <div className={`${styles.cardIn}`}>
-          <div className={`anim ${styles.img}`}>
-            <Lottie
-              options={{ ...defaultOptions, animationData: web }}
-              height={"100%"}
-              width={"100%"}
-              style={{ overflow: "visible" }}
-            />
-          </div>
-          <h2 className={`${styles.title}`}> front end</h2>
-          <p className={`${styles.text}`}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad voluptatem cumque exercitationem facilis soluta
-            magnam accusamus harum, perferendis consectetur est nostrum voluptate laudantium ex doloribus veritatis,
-            incidunt officiis assumenda in.
-          </p>
-        </div>
-        <div className={`${styles.cardIn}`}>
-          <div className={`anim ${styles.img}`}>
-            <Lottie
-              options={{ ...defaultOptions, animationData: react }}
-              height={"100%"}
-              width={"100%"}
-              style={{ overflow: "visible" }}
-            />
-          </div>
-          <h2 className={`${styles.title}`}> front end</h2>
-          <p className={`${styles.text}`}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad voluptatem cumque exercitationem facilis soluta
-            magnam accusamus harum, perferendis consectetur est nostrum voluptate laudantium ex doloribus veritatis,
-            incidunt officiis assumenda in.
-          </p>
-        </div>
-      </AwesomeSlider>
-    </div>
-  );
-};
