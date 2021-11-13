1cd64379bfb2f611d54faa4b28f07d542feb0307
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-11-13 04:09:49 +0000

    depoly for all

--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
+
+const loader = ()=>{
+  if (process.env.VERCEL_URL) {
+    return {
+      loader: 'imgix',
+      path: process.env.VERCEL_URL,
+    };
+  } else if (process.env.NEXT_PUBLIC_BASE_PATH) {
+    return {
+      loader: 'imgix',
+      path: `${process.env.HOST}/${process.env.NEXT_PUBLIC_BASE_PATH}`,
+    };
+  } else {
+    return {
+      loader: 'imgix',
+      path: 'localhost:3000',
+    };
+  }
+}
+
 module.exports = {
   images: {
-    loader: 'imgix',
-    path: `${process.env.HOST}/${process.env.NEXT_PUBLIC_BASE_PATH}` || 'localhost:3000',
+   ...loader(),
 
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
+//VERCEL_URL
