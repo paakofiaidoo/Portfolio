2a20d3be7e826cbed713686489f1579498415cef
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-11-13 03:49:50 +0000

    let test this too

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  images: {
-    loader: 'imgix',
-    path: process.env.NEXT_PUBLIC_BASE_PATH || 'localhost:3000',
 
-  },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
