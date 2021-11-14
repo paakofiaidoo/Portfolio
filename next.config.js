6fcc6b6964fbd20d8ed61f02f1246970c698b073
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-11-14 15:46:44 +0000

    final fix for all deploy

--- next.config.js
+++ next.config.js
@@ -1,28 +1,21 @@
 
-const loader = ()=>{
+const loader = () => {
   if (process.env.VERCEL_URL) {
-    return {
-      loader: 'imgix',
-      path: process.env.VERCEL_URL,
-    };
+    return {};
   } else if (process.env.NEXT_PUBLIC_BASE_PATH) {
     return {
-      loader: 'imgix',
-      path: `${process.env.HOST}/${process.env.NEXT_PUBLIC_BASE_PATH}`,
+      images: {
+        loader: 'imgix',
+        path: `${process.env.HOST}/${process.env.NEXT_PUBLIC_BASE_PATH}`,
+      }
     };
   } else {
-    return {
-      loader: 'imgix',
-      path: 'localhost:3000',
-    };
+    return {};
   }
 }
 
 module.exports = {
-  images: {
-   ...loader(),
-
-  },
+  ...loader(),
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
