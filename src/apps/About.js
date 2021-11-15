72c4641d6ebeb3f8fdf8781647529916864193e0
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-11-15 01:10:12 +0000

    completed about info input

--- src/apps/About.js
+++ src/apps/About.js
@@ -8,24 +8,24 @@ import {
 	faHouseUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Image from "next/image";
 import styles from "../../styles/apps/About.module.scss";
 import anim from "../../public/animations/avatar.json"
 const About = () => {
 	const about = [
 		{ fa: faCalendarDay, details: "21 years" },
 		{ fa: faHouseUser, details: "Fijai, Ghana" },
-		{
-			fa: faSchool,
-			details: "BSc Physics (electronics option) 3rd year undergrad, KNUST, Kumasi, Ghana",
-		},
+		{ fa: faSchool, details: "BSc Physics (electronics option) 3rd year undergrad, KNUST, Kumasi, Ghana", },
 		{ fa: faHeart, details: "Coding, Philosophy (Ontology and Epistemology), movies & animations, gaming" },
-		{ fa: faQuoteLeft, details: "live for the pursuit of knowledge" },
-		{ fa: faBullseye, details: "i live to solve problems " },
+		{ fa: faQuoteLeft, details: "I live for the pursuit of knowledge" },
+		{ fa: faBullseye, details: "I live to solve problems " },
 		{
-			fa: faUserTag, details: `i am highly self driven person, 
-			a thinker
-		` },
+			fa: faUserTag, details: `René Descartes said, "cogito, ergo sum",  (Latin: “I think, therefore I am),	
+so firstly I am thinking thing and a good thinker if I might add, I prioritize a good mental well being . 	
+I am a passionate person who loves to learn and seek to apply knowledge in to solutions.
+I sleep and wake up with technology on my mind always trying to be in step with the latest technology
+I am always looking for new ways to improve myself and my work.
+I work well with persons with different backgrounds and personalities in both remote and physical work environments.
+` },
 	];
 	const defaultOptions = {
 		loop: true,
@@ -41,18 +41,19 @@ const About = () => {
 				<img src="/brand/profile.png" alt="picture of me" className={`${styles.pic}`} />
 				<h2 className={`${styles.name}`}>
 					Aidoo Paa-Kofi Anderson
-					<p className={`${styles.sub}`}>Front-end web developer</p>
+					<p className={`${styles.sub}`}>Web developer, Electronics hobbist</p>
 				</h2>
 			</div>
 			<div className={`${styles.info} `}>
 
 				{about.map(({ fa, details }) => (
-					<p className={`${styles.text} `}>
+					<pre className={`${styles.text} `}>
 						<FontAwesomeIcon icon={fa} className={`icon`} />
 						{details}
-					</p>
+					</pre>
 				))}
 			</div>
+			<span>click on logo in taskbar to download resume</span>
 		</div>
 	);
 };
