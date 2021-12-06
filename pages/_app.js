be9f79e0975c7aac6d7b1ec2f53fa0c0dd0c53e2
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-12-06 09:25:36 +0000

    added api

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,60 @@
 import '../styles/globals.css'
 import '../styles/Normalizer.css'
 import '../styles/index.scss'
+import axios from "axios";
+import { useState, useEffect } from 'react';
+import { detect } from 'detect-browser';
+import { getStrapiURL } from './api';
 
 function MyApp({ Component, pageProps }) {
+  //get user ip address
+  const [ip, setIp] = useState('');
+  const [browser, setBrowser] = useState('');
+  const [location, setLocation] = useState('');
+
+  useEffect(() => {
+    const browser = detect();
+    setBrowser(browser);
+    axios.get('https://api.ipify.org?format=json')
+      .then(res => {
+        setIp(res.data.ip);
+        console.log(res.data.ip);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
+
+  useEffect(() => {
+    axios.get('https://ipapi.co/' + ip + '/json/')
+      .then(res => {
+        setLocation(res.data);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [ip]);
+
+  useEffect(() => {
+    console.log("run", getStrapiURL("/visitors"), process.env.NODE_ENV);
+
+    if (process.env.NODE_ENV === 'development') {
+      axios.post((getStrapiURL("/visitors")), {
+        location: location,
+        browser: browser,
+      })
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+
+    }
+  }, [location, browser]);
+
+
   return <Component {...pageProps} />
 }
 
