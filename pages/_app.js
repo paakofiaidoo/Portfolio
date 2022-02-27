2c91398b38e757d56856964cae2bd343a9299a1e
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-02-27 23:11:18 +0000

    test analytics

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,18 @@ import { detect } from 'detect-browser';
 import { getStrapiURL } from './api';
 import Head from 'next/head'
 
+import { initializeApp, getApps } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
+import 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 let send = false
+
 function MyApp({ Component, pageProps }) {
   //get user ip address
   const [ip, setIp] = useState('');
@@ -15,6 +26,20 @@ function MyApp({ Component, pageProps }) {
   const [location, setLocation] = useState('');
 
   useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyALKlNZmjNS1W0vj1juoQfVYyFGDUH_dvA",
+      authDomain: "my-portfolio-f28f3.firebaseapp.com",
+      projectId: "my-portfolio-f28f3",
+      storageBucket: "my-portfolio-f28f3.appspot.com",
+      messagingSenderId: "749579626406",
+      appId: "1:749579626406:web:ba146ae156124a5f729167",
+      measurementId: "G-PR9SEYTBCS"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    // analytics();
     const browser = detect();
     setBrowser(browser);
     axios.get('https://api.ipify.org?format=json')
