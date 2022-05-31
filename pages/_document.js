3f97901cbb9fbf8b00bde69b2e6305a94f82a76b
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-05-31 01:15:32 +0000

    analitys test 2

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-
                 <meta charSet="utf-8"></meta>
                 <link rel="icon" href="/brand/logo.svg" height="100%" width="100%" />
                 <title>Paa-Kofi Aidoo's Portfolio</title>
@@ -13,16 +12,15 @@ I am Paa-Kofi Aidoo,
 I am a Web Developer
 and I live for the pursuit of knowledge`} />
                 <meta name="google-site-verification" content="HYCtj1fXyFWpicxWAGkerf1QQmfGML5KlrYk5qe6q9o" />
-                <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-219339413-1">
-</script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-219339413-1');
-</script>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-219339413-1">
+                </script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments)}
+                    gtag('js', new Date());
+                    gtag('config', 'UA-219339413-1');
+                </script>
             </Head>
             <body>
                 <Main />
