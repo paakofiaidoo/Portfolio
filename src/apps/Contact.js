113a805b9229e2bc07526189d8036888b42d9e4a
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-07-10 14:13:25 +0000

    completed new project ui

--- src/apps/Contact.js
+++ src/apps/Contact.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "../../styles/apps/Contact.module.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Lottie from "react-lottie";
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
+import { Icon } from "../atoms/Icon";
 
 const Intro = () => {
   const [contacts, setContacts] = useState([]);
@@ -44,63 +43,3 @@ const Intro = () => {
 };
 
 export default Intro;
-const Icon = (index, href, animation, name, fa, link) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  return (
-    <li key={index}>
-      <a
-        target="_blank"
-        // href={href}
-        onDoubleClick={(e) => {
-          e.preventDefault();
-          window.open(href);
-        }}
-        onClick={async (e) => {
-          e.preventDefault();
-          // if avalable to compy to clipboard then do it else try the share api else show the toast with the link
-          if (navigator.clipboard) {
-            navigator.clipboard.writeText(link ? link : href);
-            toast.success("Copied to clipboard");
-          } else if (navigator.share) {
-            await navigator
-              .share({ title: name, url: link ? link : href, text: name })
-              .then(() => {
-                toast.success("Copied to clipboard");
-              })
-              .catch(() => {
-                toast.error("Error copying to clipboard");
-              });
-          } else {
-            toast.error(
-              `Error copying to clipboard, please select it link from here:
-            \n ${link ? link : href}`,
-              { duration: 10000 }
-            );
-          }
-        }}
-        rel="noreferrer"
-      >
-        {animation ? (
-          <Lottie
-            options={{ ...defaultOptions, animationData: animation }}
-            height={"100%"}
-            width={"6rem"}
-            title={name}
-            className={styles.contact}
-            // isPaused={hover === index}
-            style={{ overflow: "visible" }}
-          />
-        ) : (
-          <FontAwesomeIcon icon={fa} className={styles.contact} color="#25BBA8" alt={name} title={name} />
-        )}
-      </a>
-    </li>
-  );
-};
-// of all mans pain the greatest is to know much and to control little.
--- src/apps/Projects.js
+++ src/apps/Projects.js
@@ -1,65 +1,24 @@
 import AwesomeSlider from "react-awesome-slider";
 import styles from "../../styles/apps/Projects.module.scss";
-import stylesCos from "../../styles/apps/ProjectsCos.module.scss";
-import Browsers from "../atoms/Browsers";
+import React, { useState, useEffect } from "react";
+import { ProjectSlide } from "../atoms/ProjectSlide";
 
 const Projects = () => {
-  const projects = [
-    {
-      name: "nonymous-chat",
-      href: "nonymous-chat.vercel.app/",
-      description: "a chat where you can feel free to say whatever you want, no one knows it is you",
-      img: { imgDesk: "./projects/nonymous-chat.jpeg", imgMob: "/projects/nonymous (2).png" },
-    }, {
-      name: "juki-audio",
-      href: "juki-audio-player.vercel.app/",
-      description: "an audio player using the File System Access API",
-      img: {
-        imgDesk: "./projects/juki-aud.png", imgMob: "/projects/juki-aud-m.jpeg"
-      },
-    },
-    {
-      name: "artisanHR",
-      href: "artsan-hr.herokuapp.com",
-      description: "artisan service center",
-      img: { imgDesk: "./projects/artisan-hr.png", imgMob: "./projects/artsan-hr-mob.jpg" },
-      scrollDesk: true,
-      scrollMob: false,
-    },
-    {
-      name: "karibe valley farms",
-      href: "karibe-valley-farms.vercel.app",
-      description: "website for karibe-valley-farms",
-      img: { imgDesk: "./projects/karibe.png", imgMob: "./projects/karibe-mobi.png" },
-      scrollDesk: true,
-      scrollMob: false,
-    },
-    // {
-    //   name: "uv sterilizer",
-    //   href: "https://artsan-hr.herokuapp.com",
-    //   description: "",
-    //   img: { imgDesk: "/projects/artisan-hr.png", imgMob: "" },
-    // },
-    {
-      name: "Special relativity calculator",
-      href: "https://github.com/paakofiaidoo/Special-reletivity-app",
-      description: "a calculator to for problems in special relativity built with C++",
-      img: { imgDesk: "./projects/special relativity.png", imgMob: "" },
-      noMobile: true,
-    },
-    ,
-    {
-      name: "codepen",
-      href: "https://codepen.io/paakofiaidoo",
-      description: " this contains my playground of works",
-      img: { imgDesk: "./projects/codepen.jpeg", imgMob: "./projects/codepenMobi.jpeg" },
-    },
-  ];
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch("/api/projects")
+      .then((res) => res.json())
+      .then((data) => {
+        setProjects(data);
+      });
+
+    return () => {};
+  }, []);
   return (
     <div>
       <AwesomeSlider fillParent bullets={false} organicArrows mobileTouch cssModule={[styles]} media={[]}>
-        {projects.map(({ name, href, description, img, scrollDesk = false, scrollMob = false, noMobile = false }, i) => {
-          return ProjectSlide(name, description, href, img, scrollDesk, scrollMob, noMobile, i);
+        {projects.map(({ name, href, description, img }, i) => {
+          return ProjectSlide(name, description, href, img, i);
         })}
       </AwesomeSlider>
     </div>
@@ -67,40 +26,3 @@ const Projects = () => {
 };
 
 export default Projects;
-const ProjectSlide = (name, description, href, { imgDesk, imgMob }, scrollDesk, scrollMob, noMobile, i) => {
-    return (
-        <div className={stylesCos.slide} key={i}>
-            <div className={stylesCos.caption} data-type="caption">
-                <h3>
-                    <a target="_blank" href={href}>
-                        {name}{" "}
-                    </a>
-                </h3>{" "}
-                <span></span> : <span>{description}</span>
-            </div>
-
-            <Browsers mobile={imgMob ? imgMob : imgDesk} desktop={imgDesk}/>
-            {/* <div className={stylesCos.img}>
-                <div id={stylesCos.phone} style={{ display: noMobile ? "none" : "" }}>
-                    <div id={stylesCos.back}></div>
-                    <div id={stylesCos.front}></div>
-                    <div id={stylesCos.camera}></div>
-                    <div id={stylesCos.button}></div>
-                    <div id={stylesCos.display}>
-                        <span className={stylesCos.upperbefore}>
-                            <img className={`${scrollMob ? stylesCos.scrollMin : ""}`} src={imgMob ? imgMob : imgDesk} alt={name} />
-                        </span>
-                    </div>
-                </div>
-                <div className={stylesCos.laptop}>
-                    <div className={stylesCos.upper}>
-                        <span className={` ${stylesCos.upperbefore}`}>
-                            <img className={`${scrollDesk ? stylesCos.scroll : ""}`} src={imgDesk} alt={name} />
-                        </span>
-                    </div>
-                    <div className={stylesCos.lower}></div>
-                </div>
-            </div> */}
-        </div>
-    );
-};
