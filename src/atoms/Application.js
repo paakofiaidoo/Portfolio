df9a55f652f267791662f4fd4dff43ea41c45869
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-07-15 22:46:46 +0000

    fixed imports

--- src/atoms/Application.js
+++ src/atoms/Application.js
@@ -1,59 +1,61 @@
-import styles from "../../styles/Application.module.scss";
+import styles from "../../styles/atoms/Application.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import Lottie from "react-lottie";
-import Head from "next/head"
+import Head from "next/head";
 
 function Application({ style, children, close, layer, app = {} }) {
-  const { title, id, animation, fa } = app;
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    speed: "1s",
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  const Window = (children) => {
-    return (
-      <div className={`glass1 ${styles.app}`} style={{ zIndex: layer + 2, ...style }}>
-        <Head>
-          <title>{title}</title>
-        </Head>
-        <div className={styles.titleBar} draggable>
-          <li className={styles.title}>
-            {animation ? (
-              <div>
-                <Lottie
-                  className={`icon ${styles.icon} ${styles.linksIcon}`}
-                  options={{ ...defaultOptions, animationData: animation }}
-                  height={"100%"}
-                  width={"100%"}
-                  title={title}
-                  style={{ height: "2.5rem" }}
-                />
-              </div>
-            ) : (
-              <FontAwesomeIcon className={`icon ${styles.icon}`} title={title} icon={fa} color="#25BBA8" />
-            )}
-            {title}
-          </li>
-          <FontAwesomeIcon
-            className={`icon ${styles.close}`}
-            color="red"
-            title="Close"
-            icon={faTimes}
-            onClick={() => {
-              close(id);
-            }}
-          />
-        </div>
-        <div id="content" className={styles.content}>{children}</div>
-      </div>
-    );
-  };
+    const { title, id, animation, fa } = app;
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        speed: "1s",
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
+    const Window = (children) => {
+        return (
+            <div className={`glass1 ${styles.app}`} style={{ zIndex: layer + 2, ...style }}>
+                <Head>
+                    <title>{title}</title>
+                </Head>
+                <div className={styles.titleBar} draggable>
+                    <li className={styles.title}>
+                        {animation ? (
+                            <div>
+                                <Lottie
+                                    className={`icon ${styles.icon} ${styles.linksIcon}`}
+                                    options={{ ...defaultOptions, animationData: animation }}
+                                    height={"100%"}
+                                    width={"100%"}
+                                    title={title}
+                                    style={{ height: "2.5rem" }}
+                                />
+                            </div>
+                        ) : (
+                            <FontAwesomeIcon className={`icon ${styles.icon}`} title={title} icon={fa} color="#25BBA8" />
+                        )}
+                        {title}
+                    </li>
+                    <FontAwesomeIcon
+                        className={`icon ${styles.close}`}
+                        color="red"
+                        title="Close"
+                        icon={faTimes}
+                        onClick={() => {
+                            close(id);
+                        }}
+                    />
+                </div>
+                <div id="content" className={styles.content}>
+                    {children}
+                </div>
+            </div>
+        );
+    };
 
-  return Window(children);
+    return Window(children);
 }
 
 export default Application;
--- src/atoms/Nav.js
+++ src/atoms/Nav.js
@@ -1,98 +1,94 @@
 import Image from "next/image";
 import { useState } from "react";
-import styles from "../../styles/Nav.module.scss";
+import styles from "../../styles/atoms/Nav.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Lottie from "react-lottie";
 
 const Nav = ({ links = [], open }) => {
-	const [state, setState] = useState({
-		id: null,
-	});
-	const defaultOptions = {
-		loop: true,
-		autoplay: true,
-		rendererSettings: {
-			preserveAspectRatio: "xMidYMid slice",
-		},
-	};
-	const interactivity = {
-		mode: "cursor",
-		actions: [
-			{
-				visibility: [0, 0.2],
-				type: "stop",
-				frames: [0],
-			},
-			{
-				visibility: [0.2, 0.45],
-				type: "seek",
-				frames: [0, 45],
-			},
-			{
-				visibility: [0.45, 1.0],
-				type: "loop",
-				frames: [45, 60],
-			},
-		],
-	};
-	return (
-		<nav className={`navbar  ${styles.navbar}`}>
-			<div className={`links  ${styles.links}`}>
-				<>
-					<li>
-						<a href="/" ><Image
-							className={`icon ${styles.linksIcon}`}
-							src="/brand/vector/default-monochrome.svg"
-							alt="Paa-Kofi Aidoo logo"
-							width={60}
-							height={60}
-						/></a>
+    const [state, setState] = useState({
+        id: null,
+    });
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
+    const interactivity = {
+        mode: "cursor",
+        actions: [
+            {
+                visibility: [0, 0.2],
+                type: "stop",
+                frames: [0],
+            },
+            {
+                visibility: [0.2, 0.45],
+                type: "seek",
+                frames: [0, 45],
+            },
+            {
+                visibility: [0.45, 1.0],
+                type: "loop",
+                frames: [45, 60],
+            },
+        ],
+    };
+    return (
+        <nav className={`navbar  ${styles.navbar}`}>
+            <div className={`links  ${styles.links}`}>
+                <>
+                    <li>
+                        <a href="/">
+                            <Image className={`icon ${styles.linksIcon}`} src="/brand/vector/default-monochrome.svg" alt="Paa-Kofi Aidoo logo" width={60} height={60} />
+                        </a>
+                    </li>
 
-					</li>
+                    {links.map(({ title, isActive, id, fa, animation, speed }, i) => (
+                        <li
+                            className={`${isActive ? styles.isActive : ""}`}
+                            onClick={() => {
+                                open(id);
+                            }}
+                            key={i}
+                            onMouseEnter={() => {
+                                if (state.id !== id) {
+                                    setState((prev) => {
+                                        return { ...prev, id };
+                                    });
+                                }
+                            }}
+                            onMouseLeave={() => {
+                                setState({});
+                            }}
+                        >
+                            {animation ? (
+                                <div className={`${styles.anim}  ${styles.linksIcon}`}>
+                                    <Lottie
+                                        options={{ ...defaultOptions, animationData: animation }}
+                                        height={"100%"}
+                                        width={"100%"}
+                                        title={title}
+                                        className={styles.animation}
+                                        speed={speed ? speed : 1}
+                                        isStopped={state.id === id || isActive}
+                                        style={{ height: "2.2rem" }}
+                                        interactivity={interactivity}
+                                    />
+                                </div>
+                            ) : (
+                                <FontAwesomeIcon className={` ${styles.linksIcon}`} title={title} icon={fa} color="#25BBA8" />
+                            )}
 
-					{links.map(({ title, isActive, id, fa, animation, speed }, i) => (
-						<li
-							className={`${isActive ? styles.isActive : ""}`}
-							onClick={() => {
-								open(id);
-							}}
-							key={i}
-							onMouseEnter={() => {
-								if (state.id !== id) {
-									setState((prev) => {
-										return { ...prev, id };
-									});
-								}
-							}}
-							onMouseLeave={() => {
-								setState({});
-							}}>
-							{animation ? (
-								<div className={`${styles.anim}  ${styles.linksIcon}`}>
-									<Lottie
-										options={{ ...defaultOptions, animationData: animation }}
-										height={"100%"}
-										width={"100%"}
-										title={title}
-										className={styles.animation}
-										speed={speed ? speed : 1}
-										isStopped={state.id === id || isActive}
-										style={{ height: "2.2rem" }}
-										interactivity={interactivity}
-									/>
-								</div>
-							) : (
-								<FontAwesomeIcon className={` ${styles.linksIcon}`} title={title} icon={fa} color="#25BBA8" />
-							)}
-
-							<a href="#">{title}</a>
-						</li>
-					))}
-				</>
-				{/* <a style={{ display: "none" }} href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fpaakofiaidoo.tech&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" /></a> */}
-			</div>
-		</nav>
-	);
+                            <a href="#">{title}</a>
+                        </li>
+                    ))}
+                </>
+                {/* <a style={{ display: "none" }} href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fpaakofiaidoo.tech&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" /></a> */}
+            </div>
+        </nav>
+    );
 };
 
 export default Nav;
