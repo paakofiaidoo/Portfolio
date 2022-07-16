0fb96662b7f320c1e8be0154dc17774e2ab6d78f
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-07-16 02:57:02 +0000

    blob

--- src/apps/About.js
+++ src/apps/About.js
@@ -17,6 +17,7 @@ const About = () => {
         return () => {};
     }, []);
 
+    
     return (
         <div className={`${styles.about}`}>
             <div className={`${styles.intro} card`}>
