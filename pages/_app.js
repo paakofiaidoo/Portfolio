a8009910a7c44172ccea6e24595e1d06e23c686f
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-07-29 01:40:41 +0000

    test iphone fix

--- pages/_app.js
+++ pages/_app.js
@@ -45,6 +45,8 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
                 <title>Paa-Kofi Aidoo</title>
             </Head>
             <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <meta charSet="utf-8" />
-                <link rel="icon" href="/brand/logo.svg" height="100%" width="100%" />
+                <link rel="icon" href="/brand/logo.svg"  />
                 <meta
                     name="description"
                     content={`Hi 👋,
@@ -15,7 +15,6 @@ I am a Web Developer
 and I live for the pursuit of knowledge`}
                 />
                 <meta name="google-site-verification" content="HYCtj1fXyFWpicxWAGkerf1QQmfGML5KlrYk5qe6q9o" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
             <body>
                 <Main />
