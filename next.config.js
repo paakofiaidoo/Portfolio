9d90b8d3cd4e6a3d3bbaf099d51a5d5c99c35a92
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-08-20 21:29:45 +0000

    try

--- next.config.js
+++ next.config.js
@@ -1,34 +1,38 @@
-
 const loader = () => {
-  if (process.env.VERCEL_URL) {
-    return {};
-  } else if (process.env.NEXT_PUBLIC_BASE_PATH) {
-    return {
-      images: {
-        loader: 'imgix',
-        path: `${process.env.HOST}/${process.env.NEXT_PUBLIC_BASE_PATH}`,
-      }
-    };
-  } else {
-    return {};
-  }
-}
+    if (process.env.VERCEL_URL) {
+        return {};
+    } else if (process.env.NEXT_PUBLIC_BASE_PATH) {
+        return {
+            images: {
+                loader: "imgix",
+                path: `${process.env.HOST}/${process.env.NEXT_PUBLIC_BASE_PATH}`,
+            },
+        };
+    } else {
+        return {
+            images: {
+                loader: "imgix",
+                path: "",
+            },
+        };
+    }
+};
 
 module.exports = {
-  ...loader(),
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  env: {
-    // declare here all your variables
-    BASE_URL: process.env.BASE_URL,
-    REST_URL: process.env.REST_URL,
-    API_KEY: process.env.API_KEY,
-    APP_ID: process.env.APP_ID,
-    MEASUREMENT_ID: process.env.MEASUREMENT_ID,
-    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
-    PROJECT_ID: process.env.PROJECT_ID,
-    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
-    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
-  }
-}
+    ...loader(),
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+    env: {
+        // declare here all your variables
+        BASE_URL: process.env.BASE_URL,
+        REST_URL: process.env.REST_URL,
+        API_KEY: process.env.API_KEY,
+        APP_ID: process.env.APP_ID,
+        MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+        AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+        PROJECT_ID: process.env.PROJECT_ID,
+        STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+        MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    },
+};
 //VERCEL_URL
