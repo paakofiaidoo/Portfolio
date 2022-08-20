7c22b5749969b2fe466243a8e4ac55a79fa50922
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-08-20 22:15:38 +0000

    hope

--- next.config.js
+++ next.config.js
@@ -20,11 +20,11 @@ const loader = () => {
 
 module.exports = {
     // ...loader(),
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+    // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
     env: {
         // declare here all your variables
-        BASE_URL: process.env.BASE_URL,
+        // BASE_URL: process.env.BASE_URL,
         REST_URL: process.env.REST_URL,
         API_KEY: process.env.API_KEY,
         APP_ID: process.env.APP_ID,
