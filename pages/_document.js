6a0308ce6be93f7f66ed12e6425d03214fea77d1
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-09-07 22:39:27 +0000

    googletag

--- pages/_document.js
+++ pages/_document.js
@@ -30,9 +30,25 @@ and I live for the pursuit of knowledge`}
                             ga('send', 'pageview');
   `,
                     }}
-                />{" "}
+                />
+                <Script
+                    id="googleTagManager"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5BW662B');
+  `,
+                    }}
+                />
             </Head>
             <body>
+                <noscript>
+                    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BW662B" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                </noscript>
                 <Main />
                 <NextScript />
             </body>
