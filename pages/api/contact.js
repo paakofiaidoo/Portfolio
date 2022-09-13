fdafcd68021d141bf39e2cf8ce21b3ae15f3173d
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-09-13 17:36:30 +0000

    fixed contact

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,41 +2,42 @@ import { faGithub, faTwitter, faLinkedin, faWhatsapp } from "@fortawesome/free-b
 import { faMailBulk, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default (req, res) => {
-  res.statusCode = 200;
-  res.json([
-    {
-      name: "twitter",
-      href: "https://twitter.com/PaaKofiaidoo",
-      fa: faTwitter,
-    },
-    {
-      name: "Email",
-      link: "paakofiaidoo17@gmail.com",
-      href: "mailto:paakofiaidoo17@gmail.com",
-      fa: faMailBulk,
-    },
-    {
-      name: "call",
-      link: "+233553214812",
-      href: "tel:+233553214812",
-      fa: faPhone,
-    },
-    {
-      name: "linkedin",
-      href: "https://linkedin.com/in/paakofiaidoo/",
-      fa: faLinkedin,
-    },
-    {
-      name: "whatsapp",
-      href: "https://wa.me/233202396858",
-      fa: faWhatsapp,
-    },
-    {
-      name: "github",
-      href: "https://github.com/paakofiaidoo",
-      fa: faGithub,
-    },
-  ]);
+    res.statusCode = 200;
+    res.json([
+        {
+            name: "twitter",
+            href: "https://twitter.com/paakofiaidoo",
+            fa: faTwitter,
+        },
+        {
+            name: "Email",
+            link: "paakofiaidoo17@gmail.com",
+            href: "mailto:paakofiaidoo17@gmail.com",
+            fa: faMailBulk,
+        },
+        {
+            name: "call",
+            link: "+233553214812",
+            href: "tel:+233553214812",
+            fa: faPhone,
+        },
+        {
+            name: "linkedin",
+            href: "https://linkedin.com/in/paakofiaidoo/",
+            fa: faLinkedin,
+        },
+        {
+            name: "whatsapp",
+            href: "https://wa.me/233202396858",
+            link: "233202396858",
+            fa: faWhatsapp,
+        },
+        {
+            name: "github",
+            href: "https://github.com/paakofiaidoo",
+            fa: faGithub,
+        },
+    ]);
 };
 // {
 //   //   name: "Facebook",
