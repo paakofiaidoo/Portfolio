747bf38054947192f28b5cb70991cf690ccc0607
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2023-07-01 01:14:12 +0000

    updated age

--- pages/api/about.js
+++ pages/api/about.js
@@ -13,21 +13,20 @@ export default (req, res) => {
     res.statusCode = 200;
     res.json([
         { fa: faCertificate, details: "Full Stack Developer, Cloud DevOps Engineer" },
-        { fa: faCalendarDay, details: "22 years" },
+        { fa: faCalendarDay, details: "23 years" },
         { fa: faHouseUser, details: "Sekondi-Takoradi, Ghana" },
         { fa: faLanguage, details: "English, Akan(Fanti)" },
         { fa: faSchool, details: "BSc Physics (Electronics Option), Kwame Nkrumah University of Science and Technology, Kumasi, Ghana" },
         { fa: faHeart, details: "Coding, Philosophy (Ontology and Epistemology), Movies & Animations, Gaming" },
-        { fa: faBullseye, details: "I live to solve problems and for the pursuit of knowledge" },
+        { fa: faBullseye, details: "I live for the pursuit of knowledge and to solve problems" },
         {
             fa: faUserTag,
-            details: `I am a Full-Stack developer and skilled Cloud DevOps Engineer 
-            with a passion for learning and using my knowledge to build solutions and tools. 
-            I have experience working with a variety of libraries, frameworks, and programming languages. 
-            I am dedicated to staying up-to-date with the latest technology and constantly improving my skills. 
-            In addition to my work, I also enjoy giving back to the community through mentorship and leading workshops, 
-            both virtually and in person. Problem-solving is a particular interest of mine, 
-            and I thrive in finding creative solutions to challenges. 
+            details: `I am a Full-Stack developer and skilled Cloud DevOps Engineer
+            with a passion for learning and using my knowledge to build solutions and tools.
+            I have experience working with a variety of libraries, frameworks, and programming languages.
+            I am dedicated to staying up-to-date with the latest technology and constantly improving my skills.
+            Problem-solving is a particular interest of mine,
+            and I thrive in finding creative solutions to challenges.
             As René Descartes famously stated, 'cogito, ergo sum' - I think, therefore I am,
             and as a thinking individual, I am always striving to be my best self.
         `,
