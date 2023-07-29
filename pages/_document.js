834583c68e9be72a8fcc6499dfb398d5d9a20134
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2023-07-29 17:41:41 +0000

    added clarity

--- pages/_document.js
+++ pages/_document.js
@@ -26,7 +26,7 @@ and I live for the pursuit of knowledge`}
                             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
                             })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-                            
+
                             ga('create', 'G-PR9SEYTBCS', 'auto');
                             ga('send', 'pageview');
   `,
@@ -43,6 +43,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5BW662B');
   `,
+                    }}
+                /> <Script
+                    id="clarity"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        (function (c, l, a, r, i, t, y) {
+                            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+                            t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+                            y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+                        })(window, document, "clarity", "script", "i6yv3ogkqp");
+                          `,
                     }}
                 />
             </Head>
